b25f698c84bd67ad74ebc678d8a71e3dc4d98cc9
theSuriya <thesuriya3@gmail.com>
2023-11-14 18:22:43 +0530

    d

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ async def prediction(file: UploadFile = File(...)):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8501,reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=8501,reload=True)
